9596acd0e01d4b4483b79e84af8abbf6ea0c4b54
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2019-07-28 13:06:12 -0700

    Create the issue POST request.

--- create_issue.py
+++ create_issue.py
@@ -13,6 +13,11 @@ from gidgethub.aiohttp import GitHubAPI
 async def main():
     async with aiohttp.ClientSession() as session:
         gh = GitHubAPI(session, "mohantyk", oauth_token=os.getenv("GH_AUTH"))
+        await gh.post('/repos/mohantyk/test_bot/issues',
+                      data = {
+                              'title': 'We got a problem',
+                              'body': 'Use more emoji!',
+                              })
     
 loop = asyncio.get_event_loop()
 loop.run_until_complete( main() )
